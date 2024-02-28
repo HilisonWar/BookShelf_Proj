b5d40d00a0e6fed00e9c6c8533ffa7ce583d0b4c
Glebastian <aslanovgleb@mail.ru>
2024-02-29 01:58:39 +0300

    add login page and route in controller

---
+++ resources/views/loginpage.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Document</title>
+</head>
+<body>
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+    <div class="login">
+      <div class="login-header">
+        <h1>Аутентификация</h1>
+      </div>
+      <div class="login-form">
+        <h3>Email:</h3>
+        <input type="text" placeholder="Email"/><br>
+        <h3>Пароль:</h3>
+        <input type="password" placeholder="Пароль"/>
+        <br>
+        <input type="button" value="Login" class="login-button"/>
+        <br>
+        <a class="sign-up">Войти</a>
+        <br>
+        <h6 class="no-access">Can't access your account?</h6>
+      </div>
+    </div>
+    <div class="error-page">
+      <div class="try-again">Error: Try again?</div>
+    </div>
+</body>
+</html>
